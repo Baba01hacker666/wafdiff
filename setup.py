17efa595729ca9d065faea6c16f2b2d96b6c7287
Baba01hacker666 <Baba01hacker666@users.noreply.github.com>
2026-06-11 21:22:28 +0530

    Fix setup.py author and URL

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="wafdiff",
     version="1.0.0",
-    author="Baba01hacker666",
+    author="baba01hacker",
     description="WAF inconsistency detector and payload differ",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/baba01hacker/wafdiff",
+    url="https://github.com/Baba01hacker666/wafdiff",
     packages=find_packages(),
     install_requires=[
         "requests",
